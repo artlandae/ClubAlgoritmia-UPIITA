2dee967728a4cebb907a226c9bb6407b51ee8d68
artlandae <61170842+artlandae@users.noreply.github.com>
2022-09-01 17:44:54 -0500

    ejercicio 1 ProductoVectores en JAVA

--- JAVA/ProductoVectores.java
+++ JAVA/ProductoVectores.java
@@ -1,7 +1,31 @@
 package JAVA;
+import java.util.Arrays;
+import java.util.Scanner;
 class ProductoVector{
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");        
+        
+        Scanner entrada = new Scanner(System.in); 
+        
+        System.out.println("El tamaño de los vectores a y b : ");
+        int cantidad = entrada.nextInt();
+        int[] a=new int[cantidad];   
+        for (int i=0; i<=cantidad-1;i++){
+            System.out.println();
+            a[i]=entrada.nextInt();
+        }
+        int[] b=new int[cantidad];   
+        for (int i=0; i<=cantidad-1;i++){
+            b[i]=entrada.nextInt();
+        }
+        int[] producto=new int[cantidad];   
+        for (int i=0; i<=cantidad-1;i++){
+            producto[i]=a[i]*b[i];
+        }
+
+        System.out.println( Arrays.stream(producto).sum());
+
+
+        
     }
 
 }
